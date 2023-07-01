4418aa9e5e8c58b24bc50577a6e37933e3031975
Robin Boers <robindev2019@outlook.com>
2023-07-01 16:11:01 +0200

    WIP

--- dovecot/scripts/init.sh
+++ dovecot/scripts/init.sh
@@ -1,54 +1,12 @@
 #!/bin/bash
 
 set -e
-CERTFOLDER=/etc/ssl/dovecot
-CACERT=${CERTFOLDER}/ssl-cert-snakeoil.pem
-PRIVATEKEY=${CERTFOLDER}/mail.key
-PUBLICCERT=${CERTFOLDER}/mailcert.pem
 
 info () {
     echo "[INFO] $@"
 }
 
-generateCertificate() {
-    mkdir -p ${CERTFOLDER}
-    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -subj "${CERTIFICATESUBJECT}" -keyout ${PRIVATEKEY} -out ${PUBLICCERT}
-}
-updatefile() {
-    sed -i "s/<domain>/$DOMAIN/g" $@
-    sed -i "s/<addomain>/$ADDOMAIN/g" $@
-    sed -i "s/<hostname>/$HOSTNAME/g" $@
-    sed -i "s/<dockernetmask>/$DOCKERNETMASK\/$DOCKERNETMASKLEN/g" $@
-    sed -i "s/<netmask>/$NETMASK\/$NETMASKLEN/g" $@
-    sed -i "s@<cacert>@$CACERT@g" $@
-    sed -i "s@<publiccert>@$PUBLICCERT@g" $@
-    sed -i "s@<privatekey>@$PRIVATEKEY@g" $@
-    sed -i "s@<domaincontroller>@$HOSTNAME.$DOMAIN@g" $@
-    sed -i "s@<secret>@$ADPASSWORD@g" $@
-}
-
-appSetup () {
-    echo "[INFO] setup"
-
-# TLS Settings
-#    generateCertificate
-
-    addgroup --system --gid 5000 vmail
-    adduser --system --home /srv/vmail --uid 5000 --gid 5000 --disabled-password --disabled-login vmail
-
-    ln -s /etc/dovecot/dovecot-ldap.conf.ext /etc/dovecot/dovecot-ldap-userdb.conf.ext
-
-    updatefile /etc/dovecot/dovecot-ldap.conf.ext
-
-    touch /etc/dovecot/.alreadysetup
-
-    sed -i "s@#mail_max_userip_connections = 10@mail_max_userip_connections = 30@" /etc/dovecot/conf.d/20-imap.conf
-
-}
-
 appStart () {
-    [ -f /etc/dovecot/.alreadysetup ] && echo "Skipping setup..." || appSetup
-
     /usr/sbin/dovecot -F
 }
 
